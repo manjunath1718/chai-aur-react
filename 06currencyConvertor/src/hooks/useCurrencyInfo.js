b62585ee6c43c80039c449c9b8acd4682af093ba
manjunath <betagerimanju7@gmail.com>
2025-01-31 00:46:34 +0530

    currency convertor project

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  let [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+
+  console.log(data);
+
+  return data;
+}
+//https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json
+
+export default useCurrencyInfo;
