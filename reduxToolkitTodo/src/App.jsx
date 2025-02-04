f12aca58134ddbc603c34f11f34a7f889286d697
manjunath <betagerimanju7@gmail.com>
2025-02-04 22:57:27 +0530

    redux tool kit project

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddsTodo from "./components/AddsTodo";
+import Todos from "./components/todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddsTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
